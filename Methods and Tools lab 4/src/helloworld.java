d1caa4cf16fee8cbba08223f10e963b1f2ca7f5e
Dan-Matthews19 <dama8893@colorado.edu>
2014-09-17 17:31:29 -0600

    Update helloworld.java

--- Methods and Tools lab 4/src/helloworld.java
+++ Methods and Tools lab 4/src/helloworld.java
@@ -3,6 +3,7 @@ public class helloworld {
 	
 		public static void main(String[] args) {
 			System.out.println("Hello World!"); //Display the string
+			System.out.println("How are you?");
 		}
 	}
 
